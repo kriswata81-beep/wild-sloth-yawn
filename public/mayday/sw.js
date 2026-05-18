8ddd7811322629d5026846a13392891eac4841ab
kriswata81-beep <kriswata81@gmail.com>
2026-05-18 06:26:51 -1000

    Create sw.js

---
+++ public/mayday/sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'makoa-mayday-v1';
+const urlsToCache = [
+  '/mayday',
+  '/mayday/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(names =>
+      Promise.all(
+        names.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      )
+    )
+  );
+});
